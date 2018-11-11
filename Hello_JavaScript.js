00c74ee05695ef7a4aa7736ae675f7df7573d124
Mohit Gokul Murali <43658499+mohitgokul@users.noreply.github.com>
2018-11-11 05:51:47 -0800

    Update Hello_JavaScript.js

--- Hello_JavaScript.js
+++ Hello_JavaScript.js
@@ -1,4 +1,6 @@
-//JavaScript Programming
-
-
+//JavaScript Programming Language
+//console.log is mainly used in JavaScript
 console.log("Hello World");
+
+//Or use windows.alert() which creates a alert box in the window
+windows.alert("Hello World");
