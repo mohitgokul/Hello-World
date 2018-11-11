a7ba3921559b3460f05e6ca8fb15f2dee2f0aad6
Mohit Gokul Murali <43658499+mohitgokul@users.noreply.github.com>
2018-11-11 05:52:09 -0800

    Update Hello_JavaScript.js

--- Hello_JavaScript.js
+++ Hello_JavaScript.js
@@ -2,5 +2,5 @@
 //console.log is mainly used in JavaScript
 console.log("Hello World");
 
-//Or use windows.alert() which creates a alert box in the window
-windows.alert("Hello World");
+//Or use window.alert() which creates a alert box in the window
+window.alert("Hello World");
